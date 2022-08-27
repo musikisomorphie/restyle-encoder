b6b5ebbd4c7cacff6cae93dd39f80491506884f8
“musikisomorphie” <wujiqing9@gmail.com>
2022-08-27 18:20:57 +0200

    ready for retrain  HRCE

---
+++ run_gpu5a.sh
@@ -0,0 +1,23 @@
+python scripts/train_restyle_psp.py \
+--dataset_type=rxrx19a_HRCE \
+--encoder_type=ResNetBackboneEncoder \
+--exp_dir=Experiment/non_IID/rxrx19a_HRCE_chn0 \
+--max_steps=1600000 \
+--workers=8 \
+--batch_size=8 \
+--test_batch_size=8 \
+--test_workers=8 \
+--val_interval=5000 \
+--save_interval=10000 \
+--image_interval=10000 \
+--start_from_latent_avg \
+--lpips_lambda=0 \
+--l2_lambda=50 \
+--moco_lambda=0.5 \
+--w_norm_lambda=0 \
+--input_nc=2 \
+--input_ch=0 \
+--n_iters_per_batch=1 \
+--output_size=128 \
+--train_decoder=False \
+--stylegan_weights=Data/non_IID/encoder/rxrx19a_HRCE_chn0/checkpoint/790000.pt
