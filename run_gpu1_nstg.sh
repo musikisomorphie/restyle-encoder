db3d0c3d95eaffa4a250dfb28783cef6b07f89ed
“musikisomorphie” <wujiqing9@gmail.com>
2023-02-02 15:14:26 +0100

    repeat freeze/train decoder

--- run_gpu1_nstg.sh
+++ run_gpu1_nstg.sh
@@ -1,7 +1,7 @@
 CUDA_VISIBLE_DEVICES=1 python -m scripts.train_restyle_psp \
 --dataset_type=NSTG \
 --encoder_type=ResNetBackboneEncoder \
---exp_dir=Data/NSTG/GAN/encoder2 \
+--exp_dir=Data/NSTG/GAN/encodert0 \
 --max_steps=800000 \
 --workers=8 \
 --batch_size=8 \
@@ -19,13 +19,61 @@ CUDA_VISIBLE_DEVICES=1 python -m scripts.train_restyle_psp \
 --input_ch=-1 \
 --n_iters_per_batch=1 \
 --output_size=128 \
---stylegan_weights=Data/NSTG/GAN/decoder2/checkpoint/400000.pt \
+--stylegan_weights=Data/NSTG/GAN/decoder0/checkpoint/400000.pt \
 --train_decoder
 
 CUDA_VISIBLE_DEVICES=1 python -m scripts.train_restyle_psp \
 --dataset_type=NSTG \
 --encoder_type=ResNetBackboneEncoder \
---exp_dir=Data/NSTG/GAN/encoder3 \
+--exp_dir=Data/NSTG/GAN/encodert1 \
+--max_steps=800000 \
+--workers=8 \
+--batch_size=8 \
+--test_batch_size=8 \
+--test_workers=8 \
+--val_interval=50000 \
+--save_interval=100000 \
+--image_interval=50000 \
+--start_from_latent_avg \
+--lpips_lambda=0.8 \
+--l2_lambda=40 \
+--moco_lambda=0.5 \
+--w_norm_lambda=0 \
+--input_nc=6 \
+--input_ch=-1 \
+--n_iters_per_batch=1 \
+--output_size=128 \
+--stylegan_weights=Data/NSTG/GAN/decoder1/checkpoint/400000.pt \
+--train_decoder
+
+# CUDA_VISIBLE_DEVICES=1 python -m scripts.train_restyle_psp \
+# --dataset_type=NSTG \
+# --encoder_type=ResNetBackboneEncoder \
+# --exp_dir=Data/NSTG/GAN/encodert2 \
+# --max_steps=800000 \
+# --workers=8 \
+# --batch_size=8 \
+# --test_batch_size=8 \
+# --test_workers=8 \
+# --val_interval=50000 \
+# --save_interval=100000 \
+# --image_interval=50000 \
+# --start_from_latent_avg \
+# --lpips_lambda=0.8 \
+# --l2_lambda=40 \
+# --moco_lambda=0.5 \
+# --w_norm_lambda=0 \
+# --input_nc=6 \
+# --input_ch=-1 \
+# --n_iters_per_batch=1 \
+# --output_size=128 \
+# --stylegan_weights=Data/NSTG/GAN/decoder2/checkpoint/400000.pt \
+# --train_decoder
+
+CUDA_VISIBLE_DEVICES=1 python -m scripts.train_restyle_psp \
+--dataset_type=NSTG \
+--encoder_type=ResNetBackboneEncoder \
+--exp_dir=Data/NSTG/GAN/encodert3 \
 --max_steps=800000 \
 --workers=8 \
 --batch_size=8 \
